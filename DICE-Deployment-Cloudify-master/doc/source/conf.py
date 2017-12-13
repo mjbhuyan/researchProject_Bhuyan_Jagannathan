0f03bf65a05fdbd9891cf7fb6ef15b2a1de381ea
Manasjyoti Bhuyan <manasjyoti.bhuyan@mail.polimi.it>
2017-12-13 02:13:06 +0100

    Add files via upload

---
+++ DICE-Deployment-Cloudify-master/doc/source/conf.py
@@ -0,0 +1,30 @@
+extensions = ["sphinx_tosca"]
+primary_domain = "tosca"
+source_suffix = ".rst"
+master_doc = "index"
+
+project = "DICE TOSCA Library"
+copyright = "2017, XLAB d.o.o."
+author = "XLAB d.o.o."
+
+# Dummy versions, these are set by pbr when generating docs
+version = "1.0"
+release = "1.0.0"
+
+language = None
+pygments_style = "sphinx"
+
+# Theme
+html_theme = "alabaster"
+html_theme_options = {
+    "page_width": "90em",
+    "sidebar_width": "20em",
+}
+html_sidebars = {
+    "**": [
+        "about.html",
+        "navigation.html",
+        "relations.html",
+        "searchbox.html",
+    ]
+}
---
+++ DICE-Deployment-Cloudify-master/doc/source/index.rst
@@ -0,0 +1,24 @@
+==================
+DICE TOSCA Library
+==================
+
+.. include:: ../../README.rst
+
+
+Documentation Contents
+======================
+
+.. toctree::
+   :maxdepth: 2
+
+   quickstart
+   developing
+   templates
+   api
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`search`
